3327b5ac14a3d4a6d848a227b231548435f929ac
RAD <3411502803@qq.com>
2024-06-11 11:49:39 +0800

    第1次远程提交

---
+++ Source/CPPLearning/CPPLearning.h
@@ -0,0 +1,10 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+UENUM(BlueprintType)
+namespace TestEnums {
+	enum ETestEnum {TEST_TYPE_1, TEST_TYPE_2};
+}
